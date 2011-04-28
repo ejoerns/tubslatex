adabca6399bb9bb7b518f49056f53de83fbfb07f
Enrico Jörns <e.joerns@polybot.net>
2011-04-28 16:43:32 +0200

    [beamer] Verschiedene kleine Aenderungen

---
+++ beamer/testing/test01/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -v *.log
+rm -v *.aux
+rm -v *.bbl
+# rm -v *.pdf
+rm -v *.blg
+rm -v *.out
+rm -v *.toc
+rm -v *.nav
+rm -v *.snm
+rm -v *.vrb
+rm -v *~
+rm -v *.backup
+rm -v *.tar.gz
