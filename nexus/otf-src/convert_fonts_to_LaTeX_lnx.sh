2184b4da85e0700fb733e5bdab36cfdc7cf87191
Enrico Jörns [Netbook] <none@none>
2011-05-26 23:06:54 +0200

    [nexus/otf-src] Italic otf-fonts hinzugefuegt

--- nexus/otf-src/convert_fonts_to_LaTeX_lnx.sh
+++ nexus/otf-src/convert_fonts_to_LaTeX_lnx.sh
@@ -1,23 +1,23 @@
 ## Generate NexusPro LaTeX fonts, move them to right location and make them known to the LaTeX system (pdflatex)
 # Serif
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Regular --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-Regular.otf 
-# otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Italic --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-Italic.otf 
+otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Italic --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-Italic.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-SmallCaps --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --feature=smcp --map-file=NexusProSerif.map NexusSerifPro-Regular.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Slanted --slant=.167 --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-Regular.otf 
 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Bold-Regular --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-Bold.otf 
-# otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Bold-Italic --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-Bold-Italic.otf 
+otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Bold-Italic --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-BoldItalic.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Bold-SmallCaps --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --feature=smcp --map-file=NexusProSerif.map NexusSerifPro-Bold.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSerif-Bold-Slanted --slant=.167 --warn-missing --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSerif.map NexusSerifPro-Bold.otf 
 
 # Sans Serif
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Regular --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-Regular.otf 
-#otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Italic --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-Italic.otf 
+otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Italic --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-Italic.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-SmallCaps --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --feature=smcp --map-file=NexusProSans.map NexusSansPro-Regular.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Slanted --slant=.167 --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-Regular.otf 
 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Bold-Regular --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-Bold.otf 
-#otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Bold-Italic --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-Bold-Italic.otf 
+otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Bold-Italic --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-BoldItalic.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Bold-SmallCaps --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --feature=smcp --map-file=NexusProSans.map NexusSansPro-Bold.otf 
 otftotfm --no-updmap --vendor=Fontshop --name=NexusProSans-Bold-Slanted --slant=.167 --verbose --encoding=texnansx --feature=kern --feature=liga --feature=lnum --map-file=NexusProSans.map NexusSansPro-Bold.otf 
 
@@ -49,7 +49,7 @@ texhash ~/texmf/
 
 # Enable font maps and update the font map database
 
-updmap --enable Map=NexusProSans.map
-updmap --enable Map=NexusProSerif.map
+#updmap --enable Map=NexusProSans.map
+#updmap --enable Map=NexusProSerif.map
 
 updmap
