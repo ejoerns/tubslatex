56314ba14d60b55a7204f85d1b402813f60400ca
Enrico Joerns <e.joerns@tu-bs.de>
2013-01-02 16:10:35 +0100

    [install/rawtree] Skript um Herunterladen und Entpacken des Verzeichnisses erweitert, Verzeichnis als Parameter zu uebergeben, verbesserte sudo-Behandlung

--- install/rawtree/install.sh
+++ install/rawtree/install.sh
@@ -15,7 +15,7 @@ wrn_lvl=2
 inf_lvl=3
 dbg_lvl=4
 
-# log functions
+#--- log functions
 log_n() { log $silent_lvl "NOTE: $1"; } # Always prints
 log_e() { log $err_lvl "ERROR: $1"; }
 log_w() { log $wrn_lvl "WARNING: $1"; }
@@ -28,25 +28,133 @@ log() {
   fi
 }
 
+#--- Checks for either wget or curl and stores result in 'download_cmd'
+check_downloader() {
+
+  download_cmd=""
+  # check for wget
+  status=0
+  command -v wget >/dev/null 2>&1 || status=1
+  if [ $status = 0 ]; then
+    log_d "wget found"
+    download_cmd="wget -q -O 	-"
+    return 0
+  else
+    log_d "wget not found"
+  fi
+
+  # check for curl
+  status=0
+  command -v curl >/dev/null 2>&1 || status=1
+  if [ $status = 0 ]; then
+    log_d "curl found"
+    download_cmd="curl -sS"
+    return 0
+  else
+    log_d "curl not found"
+  fi
+  
+  log_e "Download failed. Neither 'curl' nor 'wget' was found on your system."
+  exit 1
+}
+
+# check if sudo is available
+check_sudo() {
+  rootrun=""
+  if sudo -v >/dev/null 2>&1; then
+    log_d "sudo ok"
+    rootrun="sudo"
+  else
+    log_d "sudo failed"
+    # Check if user is root (might be unnecessary)
+    if ! [ $(id -u) = 0 ]; then
+      log_e "This script must be run as root" 1>&2
+      exit 1
+    fi
+  fi
+}
+
+print_usage() {
+  echo "Usage: sh install.sh [-v] filename.tds.zip"
+}
+
+process_arguments() {
+  zipurl=""
+
+  for var in "$@"
+  do
+    case "$var" in
+    "-v")
+      verbosity=4
+      log_d "verbosity on"
+      ;;
+    *)
+#       zipurl=http://enricojoerns.de/tubslatex/0.3-beta3-r1190/tubslatex_0.3-beta3-r1190.tds.zip
+      zipurl=$var
+      ;;
+    esac
+  done
+  
+  if [ -z $zipurl ]; then
+    print_usage
+    exit 1;
+  fi
+}
+
+#--- Downloads $zipurl to $unzipdir
+download_tds_zip() {
+  check_downloader
+  echo -n "Downloading zipped tds file..."
+  if $download_cmd $zipurl > $unzipdir/texlive_tubs.zip 2>$logfile; then
+    echo "done."
+  else
+    echo "failed."
+    log_e "Unable to download $zipurl."
+    exit 1
+  fi
+}
+
+#-------------------------------------------------------------------------------
+
+process_arguments $@
+
+check_sudo
+
 # generate temporary logfile
-logfile=$(mktemp -p /tmp texlive-tubs.XXXXXXXX)
+logfile=$(mktemp -p /tmp texlive-tubs.log.XXXXXXXX)
 chmod 777 $logfile
 
-# Handle verbosity option
-if [ ! -z $1 ]; then
-  if [ "$1" = "-v" ]; then
-    verbosity=4
-    log_d "verbosity on"
-  else
-    log_w "Unknown parameter: $1"
-  fi
+
+
+#-------------------------------------------------------------------------------
+
+
+# create tmpdir
+unzipdir=$(mktemp -d /tmp/texlive-tubs.XXXXXXXX)
+
+# download
+if [ ! -f $zipurl ]; then
+  log_d "File $zipurl not found local, trying to download"
+  download_tds_zip
+else
+  log_d "File $zipurl found local"
+  cp $zipurl $unzipdir/texlive_tubs.zip || log_e "Copying zip failed"
 fi
 
-# Make sure only root can run our script
-if ! [ $(id -u) = 0 ]; then
-  log_e "This script must be run as root" 1>&2
-  exit 1
+# extract
+echo -n "Extracting..."
+if unzip $unzipdir/texlive_tubs.zip -d $unzipdir >$logfile 2>&1; then
+	echo "done."
+else
+	echo "failed."
+	log_e "Unable to extract zip archive $unzipdir/texlive_tubs.zip"
+	exit 1
 fi
+rm -f $unzipdir/texlive_tubs.zip
+
+exit 0
+
+
 
 # Check if tlmgr is available
 status=0
@@ -67,7 +175,7 @@ if [ $status = 0 ]; then
   case $yesno in
     y|Y)
       echo -n "Running apt-get. This may take some time..."
-      if apt-get -y install texlive-base texlive-latex-extra \
+      if $rootrun apt-get -y install texlive-base texlive-latex-extra \
           texlive-fonts-extra latex-beamer texlive-fonts-recommended \
           texlive-lang-german texlive-extra-utils cm-super | tee $logfile; then
         echo "done."
@@ -86,7 +194,13 @@ fi
 
 
 # get TEXMFLOCAL path
-texmfdir=$(kpsewhich --var-value TEXMFLOCAL || echo "TEXMFLOCAL not found")
+if texmfdir=$(kpsewhich --var-value TEXMFLOCAL; then
+  log_d "TEXMFLOCAL found: $texmfdir"
+else
+  # TODO: handle?
+  log_e "TEXMFLOCAL not found!"
+  exit 1
+fi
 
 if [ ! -d doc ] || [ ! -d fonts ] || [ ! -d tex ]; then
 	log_e "Did not find all required directories"
@@ -95,14 +209,16 @@ else
 	log_d "directories ok"
 fi
 
-echo -n "Copying files "
-cp -r doc fonts tex $texmfdir/.
-echo "[DONE]"
-
-# exit 0
+echo -n "Copying files to TEXMFLOCAL..."
+if cp -r $unzipdir/doc $unzipdir/fonts $unzipdir/tex $texmfdir/.; then
+  echo "done."
+else
+  echo "failed."
+  exit 1
+fi
 
 echo -n "Running mktexlsr..."
-if mktexlsr >> $logfile 2>&1; then
+if $rootrun mktexlsr >> $logfile 2>&1; then
   echo "done."
 else 
   echo "failed. Output has been stored in"
@@ -133,7 +249,7 @@ if [ -z `echo $pdftexmap | grep /var/lib` ]; then
   case $yesno in
     y|Y)
       echo -n "Deleting $pdftexmap..."
-      rm -f $pdftexmap
+      $rootrun rm -f $pdftexmap
       echo "done."
       ;;
     *)
@@ -145,8 +261,8 @@ fi
 
 # Touch 10local.cfg to prevent errors of buggy updmap
 # TODO: This step might be skipped in texlive 201x
-mkdir -p /etc/texmf/updmap.d || true
-touch /etc/texmf/updmap.d/10local.cfg || true
+$rootrun mkdir -p /etc/texmf/updmap.d || true
+$rootrun touch /etc/texmf/updmap.d/10local.cfg || true
       
 # Check for getnonfreefonts and install if needed
 # Is either located at /usr/bin/getnonfreefonts (as symlink)
@@ -159,7 +275,7 @@ if ([ ! -x /usr/bin/getnonfreefonts ] && [ ! -x /usr/local/bin/getnonfreefonts ]
   if [ $yesno = "y" ]; then
     echo -n "Downloading and installing getnonfreefonts..."
     if wget -P /tmp http://tug.org/fonts/getnonfreefonts/install-getnonfreefonts >> $logfile 2>&1; then
-      texlua /tmp/install-getnonfreefonts >> $logfile 2>&1
+      $rootrun texlua /tmp/install-getnonfreefonts >> $logfile 2>&1
       rm -f /tmp/install-getnonfreefonts
     echo "done."
     else
@@ -175,7 +291,7 @@ if ([ ! -x /usr/bin/getnonfreefonts ] && [ ! -x /usr/local/bin/getnonfreefonts ]
 fi
 
 echo -n "Running getnonfreefonts to install arial-urw..."
-if getnonfreefonts --sys --verbose arial-urw >> $logfile 2>&1; then
+if $rootrun getnonfreefonts --sys --verbose arial-urw >> $logfile 2>&1; then
   echo "done."
 else
   echo "failed. Output has been stored in"
@@ -193,7 +309,7 @@ npserif=`updmap-sys --listmaps 2>&1 | grep NexusProSerif | grep -v ! | grep -v d
 # Enable Nexus maps
 if [ -z "$npsans" ]; then
   echo -n "Adding map file for NexusProSans..."
-  if updmap-sys --nomkmap --nohash --enable Map=NexusProSans.map >> $logfile 2>&1; then
+  if $rootrun updmap-sys --nomkmap --nohash --enable Map=NexusProSans.map >> $logfile 2>&1; then
     echo "done."
   else
     echo "failed.\n Output has been stored in"
@@ -205,7 +321,7 @@ else
 fi
 if [ -z "$npserif" ]; then
   echo -n "Adding map file for NexusProSerif..."
-  if updmap-sys --nomkmap --nohash --enable Map=NexusProSerif.map >> $logfile 2>&1; then
+  if $rootrun updmap-sys --nomkmap --nohash --enable Map=NexusProSerif.map >> $logfile 2>&1; then
     echo "done."
   else
     echo "failed.\n Output has been stored in"
@@ -218,7 +334,7 @@ fi
 
 # Run updmap to update font db
 echo -n "Running updmap-sys..."
-if updmap-sys >> $logfile 2>&1; then
+if $rootrun updmap-sys >> $logfile 2>&1; then
   echo "done."
 else
   echo "failed. Output has been stored in"
