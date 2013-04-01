e6ede159bac66f041ed78168b9334e3b0209ce30
Enrico Joerns <e.joerns@tu-bs.de>
2013-04-01 18:14:16 +0200

    makesymlinks Skript verlinkt jetzt auch Dokumentation

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,6 +8,8 @@ ln -s $(pwd)/report ~/texmf/tex/latex/report
 ln -s $(pwd)/letter ~/texmf/tex/latex/letter
 # Nexus
 ln -s $(pwd)/nexus ~/texmf/tex/latex/nexus
-
+# Doc
+mkdir -p ~/texmf/doc/latex
+ln -s $(pwd)/doc/tubsdocguide.pdf ~/texmf/doc/latex/tubsdocguide.pdf
 # Update local ls-R
 mktexlsr ~/texmf
