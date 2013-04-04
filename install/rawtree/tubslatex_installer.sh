eebb6cef9040cde415e39d149b1464f10d144ef2
Enrico Joerns <e.joerns@tu-bs.de>
2013-04-04 14:23:32 +0200

    [install/rawtree] tubslatex_installer: fixed echo issues

--- install/rawtree/tubslatex_installer.sh
+++ install/rawtree/tubslatex_installer.sh
@@ -8,6 +8,8 @@
 # Halt on error
 set -e
 
+ECHO=/bin/echo
+
 error_occured=0
 
 exec 3>&2 # logging stream (file descriptor 3) defaults to STDERR
@@ -27,9 +29,9 @@ log_d() { log $dbg_lvl "DEBUG: $1"; }
 log() {
   if [ $verbosity -ge $1 ]; then
     # Expand escaped characters, wrap at 70 chars, indent wrapped lines
-    echo "$2" | fold -w80 -s >&3 || true
+    $ECHO "$2" | fold -w80 -s >&3 || true
   fi
-  echo "$2" | fold -w80 -s >> $logfile || true
+  $ECHO "$2" | fold -w80 -s >> $logfile || true
 }
 
 check_os() {
@@ -96,16 +98,16 @@ check_sudo() {
 
 #--- shows short usage page
 print_usage() {
-  echo "tubslatex Installer"
-  echo ""
-  echo "Syntax: sh install.sh [option] <path/to/tubslatex.tds.zip>"
-  echo ""
-  echo "where <path/to/tubslatex.tds.zip> can be both"
-  echo "a local and a remote file (http(s)/ftp)"
-  echo ""
-  echo "OPTIONS:"
-  echo "    -h    Show this help page"
-  echo "    -d    Show Debug output"
+  $ECHO "tubslatex Installer"
+  $ECHO ""
+  $ECHO "Syntax: sh install.sh [option] <path/to/tubslatex.tds.zip>"
+  $ECHO ""
+  $ECHO "where <path/to/tubslatex.tds.zip> can be both"
+  $ECHO "a local and a remote file (http(s)/ftp)"
+  $ECHO ""
+  $ECHO "OPTIONS:"
+  $ECHO "    -h    Show this help page"
+  $ECHO "    -d    Show Debug output"
 }
 
 #--- Processes command line arguments
@@ -133,11 +135,11 @@ process_arguments() {
 #--- Downloads $zipurl to $unzipdir
 download_tds_zip() {
   check_downloader
-  echo -n "Downloading zipped tds file..."
+  $ECHO -n "Downloading zipped tds file..."
   if $download_cmd $zipurl > $unzipdir/texlive_tubs.zip 2>>$logfile; then
-    echo "done."
+    $ECHO "done."
   else
-    echo "failed."
+    $ECHO "failed."
     log_e "Unable to download $zipurl."
     exit 1
   fi
@@ -151,19 +153,19 @@ check_previous_tubslatex() {
     # Checks for installation directory
     case $tubsartcl in
       $(kpsewhich --var-value TEXMFLOCAL || "null")*)
-        echo "Installed in TEXMFLOCAL"
+        $ECHO "Installed in TEXMFLOCAL"
       ;;
       $(kpsewhich --var-value TEXMFHOME || "null")*)
-        echo "Installed in TEXMFHOME"
+        $ECHO "Installed in TEXMFHOME"
       ;;
       $(kpsewhich --var-value TEXMFDIST || "null")*)
-        echo "Installed in TEXMFDIST"
+        $ECHO "Installed in TEXMFDIST"
       ;;
       $(kpsewhich --var-value TEXMFMAIN || "null")*)
-        echo "Installed in TEXMFMAIN"
+        $ECHO "Installed in TEXMFMAIN"
       ;;
       *)
-      echo "Installed in $(echo $tubsartcl | sed 's/tubsartcl.cls//g' || true)"
+      $ECHO "Installed in $($ECHO $tubsartcl | sed 's/tubsartcl.cls//g' || true)"
       ;;
     esac
     read -p "Do yout want to overwrite it? (y/n)" yesno
@@ -171,7 +173,7 @@ check_previous_tubslatex() {
       y|Y)
         ;;
       *)
-        echo "Nothing will be done."
+        $ECHO "Nothing will be done."
         exit 1
         ;;
     esac
@@ -198,7 +200,8 @@ if [ -z $zipurl ]; then
       zipurl=http://enricojoerns.de/tubslatex/tubslatex_latest_stable.tds.zip
       ;;
     *)
-      echo "Nothing will be done.\nUse -h option for usage information"
+      $ECHO "Nothing will be done."
+      $ECHO "Use -h option for usage information"
       exit 1
       ;;
   esac
@@ -220,11 +223,11 @@ else
 fi
 
 # extract
-echo -n "Extracting..."
+$ECHO -n "Extracting..."
 if unzip -o $unzipdir/texlive_tubs.zip -d $unzipdir >> $logfile 2>&1; then
-	echo "done."
+	$ECHO "done."
 else
-	echo "failed."
+	$ECHO "failed."
 	log_e "Unable to extract zip archive $unzipdir/texlive_tubs.zip"
 	rm -rf $unzipdir || true
 	exit 1
@@ -247,17 +250,17 @@ if [ "$OS_NAME" = "Linux" ]; then
 status=0
 command -v apt-get >/dev/null 2>&1 || status=1
 if [ $status = 0 ]; then
-  echo "apt-get was found on your system."
+  $ECHO "apt-get was found on your system."
   read -p "Do you want to install required dependencies now? [y/n]" yesno
   case $yesno in
     y|Y)
-      echo -n "Running apt-get. This may take some time..."
+      $ECHO -n "Running apt-get. This may take some time..."
       if $rootrun apt-get -y install texlive-base texlive-latex-extra \
           texlive-fonts-extra latex-beamer texlive-fonts-recommended \
           texlive-lang-german texlive-extra-utils cm-super | tee $logfile; then
-        echo "done."
+        $ECHO "done."
       else
-        echo "failed."
+        $ECHO "failed."
         error_occured=1
       fi
       ;;
@@ -289,48 +292,48 @@ else
   log_d "directories ok"
 fi
 
-echo -n "Copying files to TEXMFLOCAL..."
+$ECHO -n "Copying files to TEXMFLOCAL..."
 if $rootrun cp -r $unzipdir/doc $unzipdir/fonts $unzipdir/tex $texmfdir/.; then
-  echo "done."
+  $ECHO "done."
   rm -rf $unzipdir || true
 else
-  echo "failed."
+  $ECHO "failed."
   rm -rf $unzipdir || true
   exit 1
 fi
 
-echo -n "Running mktexlsr..."
+$ECHO -n "Running mktexlsr..."
 if $rootrun mktexlsr >> $logfile 2>&1; then
-  echo "done."
+  $ECHO "done."
 else 
-  echo "failed. Output has been stored in"
-  echo "$logfile"
-  echo "Please include this file if you report a bug."
+  $ECHO "failed. Output has been stored in"
+  $ECHO "$logfile"
+  $ECHO "Please include this file if you report a bug."
 	error_occured=1
 fi
 
-echo -n "Checking fontmap..."
+$ECHO -n "Checking fontmap..."
 if pdftexmap=`kpsewhich pdftex.map` >> $logfile 2>&1; then
-  echo "done."
-  echo "Found map file: $pdftexmap" >> $logfile
+  $ECHO "done."
+  $ECHO "Found map file: $pdftexmap" >> $logfile
 else
-  echo "failed. Output has been stored in"
-  echo "$logfile"
-  echo "Please include this file if you report a bug."
+  $ECHO "failed. Output has been stored in"
+  $ECHO "$logfile"
+  $ECHO "Please include this file if you report a bug."
   exit 1
   error_occured=1
 fi
     
-if [ -z `echo $pdftexmap | grep /var/lib` ]; then
-  echo ""
-  echo "Warning: You seem to use a local font map\n  ($pdftexmap)."
-  echo "Only global font maps are supported in this installation"
+if [ -z `$ECHO $pdftexmap | grep /var/lib` ]; then
+  $ECHO ""
+  $ECHO "Warning: You seem to use a local font map\n  ($pdftexmap)."
+  $ECHO "Only global font maps are supported in this installation"
   read -p "Do you want to delete local font map? [y/n]" yesno
   case $yesno in
     y|Y)
-      echo -n "Deleting $pdftexmap..."
+      $ECHO -n "Deleting $pdftexmap..."
       $rootrun rm -f $pdftexmap
-      echo "done."
+      $ECHO "done."
       ;;
     *)
       log_w "You may need to run updmap manually after install!"
@@ -350,36 +353,36 @@ command -v getnonfreefonts >/dev/null 2>&1 || status=1
 if [ $status = 0 ]; then
   log_d "getnonfreefonts found"
 else
-  echo ""
-  echo "'getnonfreefonts' was not found on your system."
-  echo "Do you want automatically download and install it? [y/n]"
+  $ECHO ""
+  $ECHO "'getnonfreefonts' was not found on your system."
+  $ECHO "Do you want automatically download and install it? [y/n]"
   read yesno
   if [ $yesno = "y" ]; then
-    echo -n "Downloading and installing getnonfreefonts..."
+    $ECHO -n "Downloading and installing getnonfreefonts..."
     if wget -P /tmp http://tug.org/fonts/getnonfreefonts/install-getnonfreefonts >> $logfile 2>&1; then
       $rootrun texlua /tmp/install-getnonfreefonts >> $logfile 2>&1
       rm -f /tmp/install-getnonfreefonts
-    echo "done."
+    $ECHO "done."
     else
-      echo "failed. Output has been stored in"
-      echo "$logfile"
-      echo "Please include this file if you report a bug."
-      echo "-- You may need to install Arial later manually --"
+      $ECHO "failed. Output has been stored in"
+      $ECHO "$logfile"
+      $ECHO "Please include this file if you report a bug."
+      $ECHO "-- You may need to install Arial later manually --"
     fi
   else
-    echo "Warning: Arial will not be installed!"
-    echo "-- You may need to install Arial later manually --"
+    $ECHO "Warning: Arial will not be installed!"
+    $ECHO "-- You may need to install Arial later manually --"
   fi
 fi
 
-echo -n "Running getnonfreefonts to install arial-urw..."
+$ECHO -n "Running getnonfreefonts to install arial-urw..."
 if $rootrun getnonfreefonts --sys --verbose arial-urw >> $logfile 2>&1; then
-  echo "done."
+  $ECHO "done."
 else
-  echo "failed. Output has been stored in"
-  echo "$logfile"
-  echo "Please include this file if you report a bug."
-  echo "-- You may need to install Arial later manually --"
+  $ECHO "failed. Output has been stored in"
+  $ECHO "$logfile"
+  $ECHO "Please include this file if you report a bug."
+  $ECHO "-- You may need to install Arial later manually --"
   error_occured=1
 fi
 
@@ -390,47 +393,47 @@ npserif=`updmap-sys --listmaps 2>&1 | grep NexusProSerif | grep -v ! | grep -v d
 
 # Enable Nexus maps
 if [ -z "$npsans" ]; then
-  echo -n "Adding map file for NexusProSans..."
+  $ECHO -n "Adding map file for NexusProSans..."
   if $rootrun updmap-sys --nomkmap --nohash --enable Map=NexusProSans.map >> $logfile 2>&1; then
-    echo "done."
+    $ECHO "done."
   else
-    echo "failed.\n Output has been stored in"
-    echo "$logfile"
-    echo "Please include this file if you report a bug."
+    $ECHO "failed.\n Output has been stored in"
+    $ECHO "$logfile"
+    $ECHO "Please include this file if you report a bug."
   fi
 else
-  echo "NexusProSans already enabled, skipping."
+  $ECHO "NexusProSans already enabled, skipping."
 fi
 if [ -z "$npserif" ]; then
-  echo -n "Adding map file for NexusProSerif..."
+  $ECHO -n "Adding map file for NexusProSerif..."
   if $rootrun updmap-sys --nomkmap --nohash --enable Map=NexusProSerif.map >> $logfile 2>&1; then
-    echo "done."
+    $ECHO "done."
   else
-    echo "failed.\n Output has been stored in"
-    echo "$logfile"
-    echo "Please include this file if you report a bug."
+    $ECHO "failed.\n Output has been stored in"
+    $ECHO "$logfile"
+    $ECHO "Please include this file if you report a bug."
   fi
 else
-  echo "NexusProSerif already enabled, skipping."
+  $ECHO "NexusProSerif already enabled, skipping."
 fi
 
 # Run updmap to update font db
-echo -n "Running updmap-sys..."
+$ECHO -n "Running updmap-sys..."
 if $rootrun updmap-sys >> $logfile 2>&1; then
-  echo "done."
+  $ECHO "done."
 else
-  echo "failed. Output has been stored in"
-  echo "$logfile"
-  echo "Please include this file if you report a bug."
+  $ECHO "failed. Output has been stored in"
+  $ECHO "$logfile"
+  $ECHO "Please include this file if you report a bug."
   error_occured=1
 fi
 
 if [ error_occured = 1 ]; then
   log_w "An error occured during installation."
-  echo "See log $logfile for further information"
+  $ECHO "See log $logfile for further information"
 else
-  echo "---"
-  echo "tubslatex was installed successfully"
+  $ECHO "---"
+  $ECHO "tubslatex was installed successfully"
   log_d "Output has been stored in $logfile"
 fi;
 
