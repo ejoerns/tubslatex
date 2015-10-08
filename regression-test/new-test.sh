59bc4d0c113c87279aa0bebb866577bf9731dc43
Enrico Joerns <e.joerns@tu-bs.de>
2015-10-08 23:36:29 +0200

    [wip] Added new regression test using comparepdf
    Signed-off-by: Enrico Joerns <e.joerns@tu-bs.de>
    
    Added initial .travis.yml
    
    Signed-off-by: Enrico Joerns <e.joerns@tu-bs.de>
    
    regression-tests: Fix date
    
    Regressions tests must not use date generated by \today as this may
    change every day.
    
    Signed-off-by: Enrico Joerns <e.joerns@tu-bs.de>
    
    regression-test: improved test script
    
    Signed-off-by: Enrico Joerns <e.joerns@tu-bs.de>
    
    .travis.yml: continue trying installing comparepdf
    
    Signed-off-by: Enrico Joerns <e.joerns@tu-bs.de>
    
    regression-test: use pdflatex instead of latexmk
    
    Signed-off-by: Enrico Joerns <e.joerns@tu-bs.de>

---
+++ regression-test/new-test.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#set -e
+
+LOGFILE=$(mktemp)
+if [ $? -ne 0 ]; then
+  echo "Creating log file failed."
+  exit 1
+fi
+
+# generate 
+cd src
+mkdir -p build
+for i in $(find . -name '*.tex'); do
+  echo -n "Generating $i..."
+  latexmk -pdf -interaction=batchmode -shell-escape -outdir=../build $i >> $LOGFILE 2>&1
+  #pdflatex -interaction batchmode -shell-escape -output-directory ../build $i >> $LOGFILE 2>&1
+  if [ $? -ne 0 ]; then
+    echo "failed."
+  else
+    echo "done."
+  fi
+done
+cd ..
+
+# compare
+
+ERROR=0
+for i in $(find src/ -name '*.tex'); do
+  echo -n "Checking $i: "
+  comparepdf -ca build/$(basename $i .tex).pdf ref/$(basename $i .tex).pdf >> $LOGFILE 2>&1
+  RESULT=$?
+  if [ $RESULT -ne 0 ]; then
+    echo "[ERROR] ($RESULT)"
+    ERROR=1
+  else
+    echo "[OK]"
+  fi
+done
+
+echo "Log dumped to $LOGFILE"
+
+exit $ERROR
